e72d5409e3f55ab4897968d437cd2371426e071e
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2024-05-08 11:37:55 -0300

    new feat(Insert do gabumon e suas evoluções)

--- db/script.sql
+++ db/script.sql
@@ -25,8 +25,20 @@ CREATE TABLE historico (
     FOREIGN KEY (winner_id) REFERENCES digimon(id)  
 );
 
-INSERT INTO digimon ('nome, nivel, tipo, ataqueEspecial, dano, HP, defesa, velocidade, campo') VALUES 
+INSERT INTO digimon (nome, nivel, tipo, ataqueEspecial, dano, HP, defesa, velocidade, campo) VALUES 
 ('Agumon', 1, 'vacina', 'Baby Flame', 60, 70, 12, 10, 'dragon roar'),
 ('Greymon', 2, 'vacina', 'Mega Flame', 112, 120, 15, 7, 'dragon roar'),
 ('MetalGreymon', 3, 'vacina', 'Giga Destroyer', 150, 160, 20, 6, 'dragon roar'),
 ('WarGreymon', 4, 'vacina', 'Gaia Force', 220, 250, 24, 15, 'dragon roar');
+
+INSERT INTO digimon (nome, nivel, tipo, ataqueEspecial, dano, HP, defesa, velocidade, campo) VALUES 
+('Gabumon', 1, 'dado', 'Petit Fire', 50, 60, 10, 12, 'Metal Empire'),
+('Garurumon', 2, 'vacina', 'Fox Fire', 108, 100, 13, 15, 'Metal Empire'),
+('WereGarurumon', 3, 'vacina', 'Kaiser Nail', 130, 140, 18, 17, 'Metal Empire'),
+('MetalGarurumon', 4, 'dado', 'Cocytus Breath', 190, 245, 27, 20, 'Metal Empire');
+
+INSERT INTO digimon (nome, nivel, tipo, ataqueEspecial, dano, HP, defesa, velocidade, campo) VALUES 
+('Palmon', 1, 'dado', 'Poison Ivy', 50, 60, 10, 12, 'Jungle Troopers'),
+('Garurumon', 2, 'vacina', 'Fox Fire', 108, 100, 13, 15, 'Metal Empire'),
+('WereGarurumon', 3, 'vacina', 'Kaiser Nail', 130, 140, 18, 17, 'Metal Empire'),
+('MetalGarurumon', 4, 'dado', 'Cocytus Breath', 190, 245, 27, 20, 'Metal Empire');
