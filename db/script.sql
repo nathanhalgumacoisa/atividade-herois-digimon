5e64bef52b31089285d3cef34f51e35adf082b73
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2024-05-08 10:26:46 -0300

    new feat(INSERT do agumon e suas evoluções.)

--- db/script.sql
+++ db/script.sql
@@ -6,6 +6,7 @@ CREATE TABLE digimon (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
     nivel INTEGER NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
     ataqueEspecial VARCHAR(50) NOT NULL,
     dano INTEGER NOT NULL,
     HP INTEGER NOT NULL,
@@ -24,3 +25,8 @@ CREATE TABLE historico (
     FOREIGN KEY (winner_id) REFERENCES digimon(id)  
 );
 
+INSERT INTO digimon (nome, nivel, tipo, ataqueEspecial, dano, HP, defesa, velocidade, campo) VALUES 
+("Agumon", 1, "vacina", "Baby Flame", 60, 70, 12, 10, "dragon roar"),
+("Greymon", 2, "vacina", "Mega Flame", 112, 120, 15, 7, "dragon roar"),
+("MetalGreymon", 3, "vacina", "Giga Destroyer", 150, 160, 20, 6, "dragon roar"),
+("WarGreymon", 4, "vacina", "Gaia Force", 220, 250, 24, 15, "dragon roar")
